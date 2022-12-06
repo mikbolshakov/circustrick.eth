e930787dfa407c8374524d82167a62cda07a17a8
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2022-12-06 21:56:37 +0300

    connect metamask

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./index.css";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 function App() {
-    function connectMetaMask() {
-        alert("connecting soon");
-    }
+    const [userAccount, setUserAccount] = useState("");
+
+    const connectMetaMask = async () => {
+        if (window.ethereum) {
+            window.ethereum
+                .request({
+                    method: "eth_requestAccounts",
+                })
+                .then((account) => {
+                    setUserAccount(account[0]);
+                });
+        } else {
+            alert("Need to install MetaMask");
+        }
+    };
+
+    const playTrick = () => {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const circusTrickAddress = "0x...";
+        // const circusTrickAbi = [...];
+
+        const circusTrick = new ethers.Contract(
+            circusTrickAddress,
+            circusTrickAbi,
+            provider
+        );
+    };
 
     return (
         <div className="App">
@@ -25,9 +51,15 @@ function App() {
                     The main thing is not to confuse the circus cashbox with the
                     magician's bottomless cylinder...
                 </h3>
-                <button onClick={connectMetaMask} className="metamask">
-                    Connect MetaMask
-                </button>
+                {userAccount ? (
+                    <button onClick={playTrick} className="metamask">
+                        Let's play!
+                    </button>
+                ) : (
+                    <button onClick={connectMetaMask} className="metamask">
+                        Connect MetaMask
+                    </button>
+                )}
             </div>
         </div>
     );
